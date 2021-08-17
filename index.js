03e0f7d54ad365c176bfbc471d040282982d8253
daint <ngothanhdai74@gmail.com>
2021-08-18 00:26:06 +0700

    sad

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 const express = require("express")
-const app = express()
+
 
 const http = require("http")
 
+const app = express()
+
 const server = http.createServer(app)
 
-// const { Server } = require("socket.io")
+const { Server } = require("socket.io")
 
-// const io = Server(server)
+const io = new Server(server)
+
+io.on("connection", socket => {
+    console.log("user connect");
+
+    socket.on("on-chat", data => {
+        console.log(data)
+        io.emit("user-chat", {
+            ...data,
+            dai: "dai"   
+        });
+    })
+
+})
 
 app.get('/dai', (req, res) => {
     res.sendFile(__dirname + "/index.html")
 })
 
 server.listen(3456, () => {
-    console.log('daint on 3456')
 })
